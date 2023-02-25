9cbf34f2a96808c112e3867067ec7e3b5f08afcb
y-dubovitsky <pXmJ5M9dvP>
2023-02-25 20:18:41 +0300

    InfoScreen improved; more information added and link on the main website

--- src/screens/info/info.screen.js
+++ src/screens/info/info.screen.js
@@ -2,16 +2,25 @@ import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ImgBackgroundComponent from "../../common/components/img-background/img-background.component";
+import { A } from '@expo/html-elements';
 
 const InfoScreen = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#FFFFFF" }}>
       <ImgBackgroundComponent>
         <View style={styles.container}>
-          <Text style={styles.text}>Memorush are effective tools for both learning and studying when they’re used correctly. Whether you’re cramming for a test or learning a new language, Memorush apps help you study smarter, not longer.
+          <Text style={styles.text}>
+            Memorush are effective tools for both learning and studying when they’re used correctly.
           </Text>
           <Text style={styles.text}>
+            Whether it's memorizing for a test or learning a new language, Memorush app helps you learn better, not longer.</Text>
+          <Text style={styles.text}>
             Whether you’re a student or a life-long learner, flashcards are a terrific way to remember information and keep the mind sharp.</Text>
+          <Text style={styles.text}>
+            For more information, visit our website:</Text>
+          <Text style={styles.link}>
+            <A href="https://memorush.ru">www.memorush.ru</A>
+          </Text>
         </View>
       </ImgBackgroundComponent>
     </SafeAreaView>
@@ -22,11 +31,11 @@ const InfoScreen = () => {
 const styles = StyleSheet.create({
   container: {
     margin: 10,
-    padding: 30,
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 10,
+    alignItems: "flex-start",
     backgroundColor: "white",
-    borderRadius: 20,
+    justifyContent: "space-around",
+    borderRadius: 10,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -38,8 +47,14 @@ const styles = StyleSheet.create({
     minWidth: 88,
   },
   text: {
-    textAlign: "center",
-    fontSize: 16
+    textAlign: "left",
+    fontSize: 20
+  },
+  link: {
+    textAlign: "left",
+    color: "#28A0DC",
+    textDecorationLine: "underline",
+    fontSize: 22
   }
 });
 
