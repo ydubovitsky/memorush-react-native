5e6dfb3d18b2639e234dd468e3d16c81f2cfa43a
y-dubovitsky <pXmJ5M9dvP>
2023-02-25 20:26:56 +0300

    Validation added to CardSetInputForm

--- src/screens/card-set-edit/atomic-components/card-set-input-form/card-set-input-form.component.js
+++ src/screens/card-set-edit/atomic-components/card-set-input-form/card-set-input-form.component.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { TextInput } from 'react-native-paper';
+import { TextInput, HelperText } from 'react-native-paper';
 
 const CardSetInputForm = ({ cardSetEntityFormInputHandler, values }) => {
+
+  const validateTitleHandler = () => {
+    return values.name.length == 0;
+  }
+
+  const validateCategoryHandler = () => {
+    return values.categoryName.length == 0;
+  }
+
   return (
     <View style={styles.container}>
       <Text>CREATE A NEW FLASHCARD SET</Text>
@@ -10,9 +19,22 @@ const CardSetInputForm = ({ cardSetEntityFormInputHandler, values }) => {
         mode="outlined"
         style={styles.input}
         label="Title"
-        value={values.title}
-        onChangeText={text => cardSetEntityFormInputHandler("title", text)}
+        value={values.name}
+        onChangeText={text => cardSetEntityFormInputHandler("name", text)}
       />
+      <HelperText type="error" visible={validateTitleHandler()}>
+        Title length must be greater than 0
+      </HelperText>
+      <TextInput
+        mode="outlined"
+        style={styles.input}
+        label="Category"
+        value={values.categoryName}
+        onChangeText={text => cardSetEntityFormInputHandler("categoryName", text)}
+      />
+      <HelperText type="error" visible={validateCategoryHandler()}>
+        Category name must be greater than 0
+      </HelperText>
       <TextInput
         mode="outlined"
         style={styles.input}
@@ -34,7 +56,7 @@ const CardSetInputForm = ({ cardSetEntityFormInputHandler, values }) => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
-    borderRadius: 20,
+    borderRadius: 10,
     padding: 10,
   },
   description: {
